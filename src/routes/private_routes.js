aa3f45fd1f9e3878eb7c5a7863295c3adf7d1378
leonelaam <77633649+Leonelaam@users.noreply.github.com>
2021-12-08 16:14:00 -0600

    auth

--- src/routes/private_routes.js
+++ src/routes/private_routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const mysqlConnection = required('../config/db.config')
+const {verifyToken} = require('..config/verifyToken');
+const {getprofileauthorization} = require('../controllers/users.controllers')
+
 
 // router.get('/',)
+router.get('/users',verifyToken,getprofileauthorization);
 
 module.exports = router;
